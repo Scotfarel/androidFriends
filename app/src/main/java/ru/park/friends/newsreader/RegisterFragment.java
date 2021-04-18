edfd8ebef41537bb9e2e53deea498d82cec33170
a.anufriev <anufriev.artem.mail@gmail.com>
2021-04-18 12:02:07 +0300

    Removed error after new input

--- app/src/main/java/ru/park/friends/newsreader/RegisterFragment.java
+++ app/src/main/java/ru/park/friends/newsreader/RegisterFragment.java
@@ -20,6 +20,7 @@ public class RegisterFragment extends Fragment {
     private RegisterViewModel authViewModel;
     private EditText email, password;
     private Button registerButton;
+    private TextView errorTextView;
 
     private TextWatcher textWatcher = new TextWatcher() {
         @Override
@@ -30,6 +31,7 @@ public class RegisterFragment extends Fragment {
 
         @Override
         public void afterTextChanged(Editable s) {
+            errorTextView.setVisibility(View.INVISIBLE);
             if (email.getText().toString().length() == 0 ||
                     password.getText().toString().length() == 0) {
                 registerButton.setEnabled(false);
@@ -49,14 +51,10 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        authViewModel = new ViewModelProvider(getActivity()).get(RegisterViewModel.class);
-        authViewModel
-                .getRegisterState()
-                .observe(getViewLifecycleOwner(), new AuthObserver(view.findViewById(R.id.register_error)));
-
         email = view.findViewById(R.id.register_email);
         password = view.findViewById(R.id.register_password);
         registerButton = view.findViewById(R.id.register_button);
+        errorTextView = view.findViewById(R.id.register_error);
 
         email.addTextChangedListener(textWatcher);
         password.addTextChangedListener(textWatcher);
@@ -69,6 +67,11 @@ public class RegisterFragment extends Fragment {
                 authViewModel.register(email.getText().toString(), password.getText().toString());
             }
         });
+
+        authViewModel = new ViewModelProvider(getActivity()).get(RegisterViewModel.class);
+        authViewModel
+                .getRegisterState()
+                .observe(getViewLifecycleOwner(), new AuthObserver(errorTextView));
 
     }
 
@@ -83,6 +86,7 @@ public class RegisterFragment extends Fragment {
         public void onChanged(RegisterState loginState) {
             if (loginState == RegisterState.NOT_EMAIL) {
                 errorTextView.setText(getResources().getString(R.string.invalid_email));
+                errorTextView.setVisibility(View.VISIBLE);
             }
         }
     }
